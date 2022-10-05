511b59824c0ec84846679cbe4293fdc6e2ad8b07
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-05 17:24:21 +0200

    password generator

--- js/script.js
+++ js/script.js
@@ -3,11 +3,6 @@ const cognomeUtente = prompt('è il tuo cognome ?');
 const coloreUtente = prompt('Qual è il tuo colore preferito ?');
 const etaUtente = prompt('Quanti anni hai ?');
 
-function validateUserName(nomeUtente){
-    var usernameRegex = /^[a-zA-Z0-9]+$/;
-    return usernameRegex.test(nomeUtente);
-  }
-
 const descrizione = ` 
 Benvenuto, tu sei ${nomeUtente} ${cognomeUtente}.
 Il tuo colore preferito è il ${coloreUtente},
@@ -18,3 +13,9 @@ const infoUtente = document.getElementById('mioId');
 
 infoUtente.innerHTML = descrizione;
 
+
+const passwordGener = "La tua password è : " + nomeUtente + cognomeUtente + coloreUtente + etaUtente;
+
+const passwordUtente = document.getElementById('myPassword');
+
+passwordUtente.innerHTML = passwordGener;
