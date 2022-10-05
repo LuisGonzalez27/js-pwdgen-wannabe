0e8f2b3a59163bc6f725658dbfb0947afccd17c8
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-05 15:31:48 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const nomeUtente = prompt('Hey, qual è il tuo nome ?');
+const cognomeUtente = prompt('è il tuo cognome ?');
+const coloreUtente = prompt('Qual è il tuo colore preferito ?');
+const etaUtente = prompt('Quanti anni hai ?');
+
+const descrizione = ` 
+Benvenuto, tu sei ${nomeUtente} ${cognomeUtente}.
+Il tuo colore preferito è il ${coloreUtente},
+la tua età  è ${etaUtente}.
+`;
+
+const infoUtente = document.getElementById('mioId');
+
+infoUtente.innerHTML = descrizione;
