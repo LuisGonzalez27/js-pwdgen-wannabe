d00ac3b367205f732fb0a681b18ebf0a533fd595
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-05 15:58:24 +0200

    test JS HTML DOM

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const cognomeUtente = prompt('è il tuo cognome ?');
 const coloreUtente = prompt('Qual è il tuo colore preferito ?');
 const etaUtente = prompt('Quanti anni hai ?');
 
+function validateUserName(nomeUtente){
+    var usernameRegex = /^[a-zA-Z0-9]+$/;
+    return usernameRegex.test(nomeUtente);
+  }
+
 const descrizione = ` 
 Benvenuto, tu sei ${nomeUtente} ${cognomeUtente}.
 Il tuo colore preferito è il ${coloreUtente},
@@ -12,3 +17,4 @@ la tua età  è ${etaUtente}.
 const infoUtente = document.getElementById('mioId');
 
 infoUtente.innerHTML = descrizione;
+
